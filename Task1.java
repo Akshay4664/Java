5f10513edd370d82ee9a1a07e88e4d1c58741838
Akshay Sharma <akshaysharma4664@gmail.com>
2022-03-22 18:56:36 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class Task1 {
+    public static void main(String[] args) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+        hs.add(1);
+        hs.add(5);
+        hs.add(3);
+        hs.add(8);
+
+        System.out.println(hs);
+    }
+}
