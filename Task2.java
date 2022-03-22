4eeae1fe349da57814005dcfd1f0bb2e5e201bfe
Akshay Sharma <akshaysharma4664@gmail.com>
2022-03-22 19:00:36 +0530

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Task2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(5);
+        list.add(3);
+        list.add(8);
+
+        System.out.println(list);
+    }
+}
